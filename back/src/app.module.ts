2f70649e2aa92499b0790b43178652c944c5adee
Paul Ohl <paul.lv.ohl@gmail.com>
2025-04-08 00:25:46 +0200

    feat: Skeleton for the main routes

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FoldersModule } from './folders/folders.module';
+import { MediasModule } from './medias/medias.module';
+import { ReactionsModule } from './reactions/reactions.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [FoldersModule, MediasModule, ReactionsModule, AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
