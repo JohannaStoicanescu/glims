6c9d76722e8174b93635e14bc4ab49d20b01f30c
Johanna <104634871+JohannaStoicanescu@users.noreply.github.com>
2025-08-12 14:38:45 +0200

    feature/GLIM-86 add ci setup - conditional ci running

--- back/eslint.config.js
+++ back/eslint.config.js
@@ -1,50 +1,50 @@
-const { defineConfig, globalIgnores } = require("eslint/config");
+const { defineConfig, globalIgnores } = require('eslint/config');
 
-const tsParser = require("@typescript-eslint/parser");
-const typescriptEslintEslintPlugin = require("@typescript-eslint/eslint-plugin");
-const globals = require("globals");
-const js = require("@eslint/js");
+const tsParser = require('@typescript-eslint/parser');
+const typescriptEslintEslintPlugin = require('@typescript-eslint/eslint-plugin');
+const globals = require('globals');
+const js = require('@eslint/js');
 
-const { FlatCompat } = require("@eslint/eslintrc");
+const { FlatCompat } = require('@eslint/eslintrc');
 
 const compat = new FlatCompat({
-	baseDirectory: __dirname,
-	recommendedConfig: js.configs.recommended,
-	allConfig: js.configs.all,
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
 module.exports = defineConfig([
-	{
-		languageOptions: {
-			parser: tsParser,
-			sourceType: "module",
+  {
+    languageOptions: {
+      parser: tsParser,
+      sourceType: 'module',
 
-			parserOptions: {
-				project: "tsconfig.json",
-				tsconfigRootDir: __dirname,
-			},
+      parserOptions: {
+        project: 'tsconfig.json',
+        tsconfigRootDir: __dirname,
+      },
 
-			globals: {
-				...globals.node,
-				...globals.jest,
-			},
-		},
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+    },
 
-		plugins: {
-			"@typescript-eslint": typescriptEslintEslintPlugin,
-		},
+    plugins: {
+      '@typescript-eslint': typescriptEslintEslintPlugin,
+    },
 
-		extends: compat.extends(
-			"plugin:@typescript-eslint/recommended",
-			"plugin:prettier/recommended",
-		),
+    extends: compat.extends(
+      'plugin:@typescript-eslint/recommended',
+      'plugin:prettier/recommended'
+    ),
 
-		rules: {
-			"@typescript-eslint/explicit-function-return-type": "error",
-			"@typescript-eslint/explicit-module-boundary-types": "off",
-			"@typescript-eslint/interface-name-prefix": "off",
-			"@typescript-eslint/no-explicit-any": "error",
-		},
-	},
-	globalIgnores(["**/.eslintrc.js"]),
+    rules: {
+      '@typescript-eslint/explicit-function-return-type': 'error',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/no-explicit-any': 'error',
+    },
+  },
+  globalIgnores(['**/.eslintrc.js']),
 ]);
