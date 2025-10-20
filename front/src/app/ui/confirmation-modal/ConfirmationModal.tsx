5dbeaeb5957d66295843a38f26b27bdf07ac3166
Johanna Perso <johanna.stoicanescu@gmail.com>
2025-10-20 17:33:55 +0200

    move confirmation modal to ui

--- front/src/app/ui/confirmation-modal/ConfirmationModal.tsx
+++ front/src/app/ui/confirmation-modal/ConfirmationModal.tsx
@@ -2,6 +2,8 @@
 
 import { useState } from 'react';
 
+import { ConfirmationModalContent } from './components/ConfirmationModalContent';
+
 interface ConfirmationModalProps {
   icon: React.ReactNode;
   title: string;
@@ -13,7 +15,7 @@ interface ConfirmationModalProps {
   radial?: boolean;
 }
 
-export function ConfirmationModal({
+export default function ConfirmationModal({
   icon,
   title,
   message,
@@ -28,37 +30,28 @@ export function ConfirmationModal({
 
   return (
     showModal && (
-      <div>
-        <div
-          onClick={() => setShowModal(false)}
-          className="fixed z-10 w-full h-full bg-black opacity-20"></div>
-        <div className="fixed z-20 m-auto inset-y-0 inset-x-0 w-11/12 h-1/2 md:w-1/2 lg:w-2/6">
-          <div
-            className={`rounded-xl p-6 md:p-10 flex flex-col items-center gap-4 
-            ${radial ? 'bg-radial from-red-100 via-white to-white bg-[length:200%_200%] bg-top' : 'bg-white'}`}>
-            <span className="text-4xl bg-red-50 text-red-500 rounded-full w-20 h-20 md:w-32 md:h-32 flex items-center justify-center">
-              {icon}
-            </span>
-            <p className="text-center font-semibold text-2xl">{title}</p>
-            <p className="text-center text-gray-600 w-11/12 md:w-2/3">
-              {message}
-            </p>
-            <div className="w-full flex items-center gap-2">
-              <button
-                type="button"
-                className="w-full flex items-center justify-center gap-2 font-bold py-3 rounded-xl border cursor-pointer transition text-black bg-white border-gray-300 hover:bg-black hover:text-white hover:border-black"
-                onClick={onCancel}>
-                {cancelButtonText}
-              </button>
-              <button
-                type="button"
-                className="w-full flex items-center justify-center gap-2 font-bold py-3 rounded-xl border cursor-pointer transition text-white bg-black border-black hover:bg-white hover:text-black"
-                onClick={onConfirm}>
-                {confirmButtonText}
-              </button>
-            </div>
-          </div>
-        </div>
+      <div
+        className="fixed z-20 m-auto inset-y-0 inset-x-0 w-11/12 h-1/2 md:w-1/2 lg:w-2/6 backdrop-blur-[1px]"
+        onClick={() => setShowModal(false)}
+        aria-labelledby="confirmation-modal-title"
+        aria-modal="true"
+        role="dialog">
+        <ConfirmationModalContent
+          icon={icon}
+          title={title}
+          message={message}
+          confirmButtonText={confirmButtonText}
+          cancelButtonText={cancelButtonText}
+          onConfirm={() => {
+            setShowModal(false);
+            onConfirm();
+          }}
+          onCancel={() => {
+            setShowModal(false);
+            onCancel();
+          }}
+          radial={radial}
+        />
       </div>
     )
   );
