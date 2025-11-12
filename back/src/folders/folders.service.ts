7edf927e5a2766abd928176a1eb8a5d400a245b1
Paul OHL <paul.lv.ohl@gmail.com>
2025-11-12 19:02:37 +0100

    feat: Added a folders module, controller and service

--- back/src/folders/folders.service.ts
+++ back/src/folders/folders.service.ts
@@ -1,87 +1,31 @@
 import { Injectable } from '@nestjs/common';
-import { Folder } from '@prisma/client';
-import { FoldersRepository } from './folders.repository';
-import { uuidv4 } from 'better-auth';
-
-export enum FoldersError {
-  FOLDER_NOT_FOUND = 'Folder not found',
-  FORBIDDEN = 'Not allowed',
-}
-
-export class FoldersException extends Error {
-  constructor(public readonly type: FoldersError) {
-    super(type);
-  }
-}
+import { Folder, Prisma } from '@prisma/client';
+import { PrismaService } from 'src/lib/prisma.service';
 
 @Injectable()
 export class FoldersService {
-  constructor(private readonly repository: FoldersRepository) {}
+  constructor(private prisma: PrismaService) {}
 
-  async getFolderById(folder_id: string, user_id: string): Promise<Folder> {
-    const folder = await this.checkFolderOwnership(folder_id, user_id);
-    return folder;
+  async getFolderById(folder_id: string): Promise<Folder | null> {
+    return this.prisma.folder.findUnique({ where: { id: folder_id } });
   }
 
   async getUserFolders(owner_id: string): Promise<Folder[]> {
-    return this.repository.getUserFolders(owner_id);
+    return this.prisma.folder.findMany({ where: { owner_id } });
   }
 
-  async createFolder(
-    data: { title: string; description?: string; password?: string },
-    user_id: string
-  ): Promise<Folder> {
-    const upload_url = uuidv4().format;
-    const download_url = uuidv4().format;
-
-    return this.repository.createFolder({
-      ...data,
-      upload_url,
-      download_url,
-      owner: { connect: { id: user_id } },
-    });
+  async createFolder(data: Prisma.FolderCreateInput): Promise<Folder> {
+    return this.prisma.folder.create({ data });
   }
 
-  async updateFolder(
-    folder_id: string,
-    user_id: string,
-    data: Partial<{
-      title: string;
-      description?: string;
-      password?: string;
-      upload_url: string;
-      download_url: string;
-    }>
-  ): Promise<Folder> {
-    await this.checkFolderOwnership(folder_id, user_id);
-    return this.repository.updateFolder({ where: { id: folder_id }, data });
+  async updateFolder(params: {
+    where: Prisma.FolderWhereUniqueInput;
+    data: Prisma.FolderUpdateInput;
+  }): Promise<Folder> {
+    return this.prisma.folder.update(params);
   }
 
-  async refreshFolderLinks(
-    user_id: string,
-    folder_id: string
-  ): Promise<Folder> {
-    await this.checkFolderOwnership(folder_id, user_id);
-
-    const upload_url = uuidv4().format;
-    const download_url = uuidv4().format;
-
-    return this.updateFolder(folder_id, user_id, { upload_url, download_url });
-  }
-
-  async deleteFolder(folder_id: string, user_id: string): Promise<Folder> {
-    await this.checkFolderOwnership(folder_id, user_id);
-    return this.repository.deleteFolder({ id: folder_id });
-  }
-
-  private async checkFolderOwnership(
-    folder_id: string,
-    owner_id: string
-  ): Promise<Folder> {
-    const folder = await this.repository.getFolderById(folder_id);
-    if (!folder) throw new FoldersException(FoldersError.FOLDER_NOT_FOUND);
-    if (folder.owner_id !== owner_id)
-      throw new FoldersException(FoldersError.FORBIDDEN);
-    return folder;
+  async deleteFolder(where: Prisma.FolderWhereUniqueInput): Promise<Folder> {
+    return this.prisma.folder.delete({ where });
   }
 }
