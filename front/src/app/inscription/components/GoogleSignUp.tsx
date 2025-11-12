be3e9d2bf14987278bae4e0268d81df7e2ed00e1
Paul OHL <paul.lv.ohl@gmail.com>
2025-11-12 16:50:13 +0100

    fix: lint + format

--- front/src/app/inscription/components/GoogleSignUp.tsx
+++ front/src/app/inscription/components/GoogleSignUp.tsx
@@ -6,6 +6,8 @@ import ButtonForm from '@/app/ui/form/ButtonForm';
 import { useAuthClient } from '@/hooks';
 
 export default function GoogleSignUp() {
+  const authClient = useAuthClient();
+
   return (
     <div className="flex flex-col gap-4">
       <ButtonForm
@@ -15,7 +17,7 @@ export default function GoogleSignUp() {
         }
         style="light"
         onClick={async () => {
-          const data = await useAuthClient().signIn.social({
+          const data = await authClient.signIn.social({
             provider: 'google',
             callbackURL: `${window.location.origin}/exemple-auth?newUser=false`,
             errorCallbackURL: `${window.location.origin}/exemple-auth?error=true`,
