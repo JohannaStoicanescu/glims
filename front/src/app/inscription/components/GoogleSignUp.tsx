900d519007a56d1255431ede0cec343a5e6c649b
Paul Ohl <37795294+paul-ohl@users.noreply.github.com>
2025-12-01 13:53:35 +0100

    Feature/glim 9/google auth (#30)
    * feat: Added google sign-in and sign-up
    
    * fix: small fixes
    
    * fix: lint + format

--- front/src/app/inscription/components/GoogleSignUp.tsx
+++ front/src/app/inscription/components/GoogleSignUp.tsx
@@ -3,8 +3,11 @@
 import { FaGoogle } from 'react-icons/fa';
 
 import ButtonForm from '@/app/ui/form/ButtonForm';
+import { useAuthClient } from '@/hooks';
 
 export default function GoogleSignUp() {
+  const authClient = useAuthClient();
+
   return (
     <div className="flex flex-col gap-4">
       <ButtonForm
@@ -13,7 +16,15 @@ export default function GoogleSignUp() {
           <span className="hidden md:block">S&apos;inscrire avec Google</span>
         }
         style="light"
-        onClick={() => {}}
+        onClick={async () => {
+          const data = await authClient.signIn.social({
+            provider: 'google',
+            callbackURL: `${window.location.origin}/exemple-auth?newUser=false`,
+            errorCallbackURL: `${window.location.origin}/exemple-auth?error=true`,
+            newUserCallbackURL: `${window.location.origin}/exemple-auth?newUser=true`,
+          });
+          console.table(data);
+        }}
       />
     </div>
   );
